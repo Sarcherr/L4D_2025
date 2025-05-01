52a0d5edadb955a9c14d5af9d142e7a66ef656e1
Sarche <sss006122@qq.com>
2025-05-01 17:05:48 +0800

    2025/5/1：添加AudioManager音效管理器

---
+++ Assets/Scripts/Test/AudioTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            AudioManager.Instance.PlaySound("Pistol_Mars712_Shot");
+        }
+    }
+}
