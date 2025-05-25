49c772c95c134d1041f77244f4bd813bcb17cad8
Sarche <sss006122@qq.com>
2025-05-26 02:14:06 +0800

    2025/5/26：部分修改RuntimeUnitData数据结构；完善AttackManager攻击/治疗处理部分

--- Assets/Scripts/MainLogic/Level/Component/Buff/Buff.cs
+++ Assets/Scripts/MainLogic/Level/Component/Buff/Buff.cs
@@ -10,7 +10,15 @@ public class BuffBase
     public string Name;
     public string BelongName;
     public string OriginName;
-    
+
+    /// <summary>
+    /// buff剩余回合数
+    /// </summary>
+    public int TurnCount;
+    /// <summary>
+    /// buff层数
+    /// </summary>
+    public int BuffCount;
 }
 
 /// <summary>
--- Assets/Scripts/MainLogic/Level/Manager/AttackManager.cs
+++ Assets/Scripts/MainLogic/Level/Manager/AttackManager.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
 /// 攻击管理器
 /// <para>处理PowerManager发送的攻击请求</para>
+/// <para>治疗行为也整合在此</para>
 /// </summary>
 public class AttackManager : Singleton<AttackManager>
 {
@@ -27,7 +29,77 @@ public class AttackManager : Singleton<AttackManager>
         Executor.ExecuteAttack(request);
     }
 
-    
+    /// <summary>
+    /// 获取攻击结果
+    /// </summary>
+    /// <param name="origin">发起者</param>
+    /// <param name="target">目标</param>
+    /// <param name="damageRate">技能伤害倍率</param>
+    /// <param name="dotChance">效果命中概率</param>
+    /// <returns>攻击结果</returns>
+    public Attack GetAttack(string origin, string target, float damageRate, float dotChance = 0f)
+    {
+        RuntimeUnitData originUnitData = ControllerManager.Instance.AllRuntimeUnitData[origin];
+        RuntimeUnitData targetUnitData = ControllerManager.Instance.AllRuntimeUnitData[target];
+
+        bool hit = CheckHit(originUnitData.CurrentHitChance, targetUnitData.CurrentDogeChance);
+        bool crit = CheckCrit(originUnitData.CurrentCritChance);
+        bool resist = CheckResistance(dotChance, targetUnitData.CurrentResistanceChance);
+
+        if(!hit)
+        {
+            return new Attack
+            {
+                IsHit = false,
+                IsCrit = false,
+                IsResist = true,
+                Damage = 0
+            };
+        }
+        else
+        {
+            // 基础伤害 = 攻击单位攻击力 * (1 + 攻击单位额外攻击力) * 技能伤害倍率 * 受击单位当前减伤率
+            int damage = (int)(originUnitData.CurrentAttack * (1 + originUnitData.CurrentExtraAttackRate)
+                * damageRate * targetUnitData.CurrentDamageReductionRate);
+            if(crit)
+            {
+                damage = (int)(damage * originUnitData.CurrentCritRate);
+            }
+
+            return new Attack
+            {
+                IsHit = true,
+                IsCrit = crit,
+                IsResist = resist,
+                Damage = damage
+            };
+        }
+    }
+    /// <summary>
+    /// 获取治疗结果
+    /// </summary>
+    /// <param name="origin">发起者</param>
+    /// <param name="target">目标</param>
+    /// <param name="healRate">技能治疗倍率</param>
+    /// <returns>治疗结果</returns>
+    public Heal Heal(string origin, string target, float healRate)
+    {
+        RuntimeUnitData originUnitData = ControllerManager.Instance.AllRuntimeUnitData[origin];
+
+        bool crit = CheckCrit(originUnitData.CurrentCritChance);
+        // 基础治疗 = 攻击单位攻击力 * (1 + 攻击单位额外攻击力) * 技能治疗倍率
+        int healValue = (int)(originUnitData.CurrentAttack * (1 + originUnitData.CurrentExtraAttackRate)
+            * healRate);
+        if (crit)
+        {
+            healValue = (int)(healValue * originUnitData.CurrentCritRate);
+        }
+        return new Heal
+        {
+            IsCrit = crit,
+            HealValue = healValue
+        };
+    }
 
     /// <summary>
     /// 检查是否命中
@@ -88,6 +160,9 @@ public class AttackExecutor
     }
 }
 
+/// <summary>
+/// 攻击结果
+/// </summary>
 public struct Attack
 {
     /// <summary>
@@ -106,4 +181,19 @@ public struct Attack
     /// 伤害值
     /// </summary>
     public int Damage;
+}
+
+/// <summary>
+/// 治疗结果
+/// </summary>
+public struct Heal
+{
+    /// <summary>
+    /// 是否暴击
+    /// </summary>
+    public bool IsCrit;
+    /// <summary>
+    /// 治疗值
+    /// </summary>
+    public int HealValue;
 }
